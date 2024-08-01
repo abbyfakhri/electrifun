7f3041c69e464eeec6270d33507d1644c178c89e
abbyfakhri <abbyfakhrichoiry@gmail.com>
2024-08-01 14:37:40 +0700

    initial commit

---
+++ Electrifun/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Electrifun
+//
+//  Created by Abby Fakhri Choiry on 01/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Electrifun/ElectrifunApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElectrifunApp.swift
+//  Electrifun
+//
+//  Created by Abby Fakhri Choiry on 01/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ElectrifunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
